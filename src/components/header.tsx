8a9c1124a91bd3a6e4deebeb4272ec013c6d914b
Marin404-dev <marvrosanto@gmail.com>
2025-11-19 19:03:22 +0800

    first commit

---
+++ src/components/header.tsx
@@ -0,0 +1,32 @@
+function Header() {
+  return (
+    <div className="bg-red-100 p-6">
+
+      <div className="bg-blue-50 justify-center text-center flex flex-row">
+        <div className="flex flex-row">
+          <p className="pl-6 pr-6">nav1</p>
+          <p className="pl-6 pr-6">nav 2</p>
+        </div>
+
+        <div>
+          <h1>unique</h1>
+        </div>
+
+        <div className="flex flex-row">
+          <p className="pl-6 pr-6">nav1</p>
+          <p className="pl-6 pr-6">nav 2</p>
+        </div>
+
+        {/* actions */}
+        <div className="flex ">
+          <p>icon</p>
+        </div>
+      </div>
+
+
+      header
+    </div>
+  )
+}
+
+export default Header
