fb94bc00f5f8b8d58a1fbd7715ce233e5ba85b70
Marin404-dev <marvrosanto@gmail.com>
2025-12-13 19:20:24 +0800

    added black border bottom if active in navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,36 +1,54 @@
+import { NavLink } from "react-router-dom";
 import uniqueLogo from "../assets/unique_logo.png";
 import { IoPersonOutline } from "react-icons/io5";
 import { HiOutlineShoppingBag } from "react-icons/hi2";
 
 function Header() {
+  const navLinksLeft = [
+    { name: "Home", path: "/" },
+    { name: "Apparel", path: "/apparel" },
+  ];
+
+  const navLinksRight = [
+    { name: "About us", path: "/about" },
+    { name: "Sale", path: "/sale" },
+  ];
+
+  const navLinkClass = ({ isActive }: { isActive: boolean }) =>
+    `pl-8 pr-8 relative py-3 ${
+      isActive
+        ? "after:absolute after:bottom-[-13px] after:left-0 after:w-12 after:h-[2px] after:bg-black after:left-1/2 after:-translate-x-1/2"
+        : ""
+    }`;
+
   return (
-    <div className="fixed top-0 left-0 right-0 z-50 bg-white flex items-center text-center p-3 border-b border-[#dadada] shadow">
-      <div className="flex flex-1 justify-center">
-        <nav className="flex flex-row self-center">
-          <a href="">
-            <p className="pl-8 pr-8">Home</p>
-          </a>
-          <a href="">
-            <p className="pl-8 pr-8">Apparel</p>
-          </a>
+    <div className="fixed top-0 left-0 right-0 z-50 bg-white flex items-center justify-center p-3 border-b border-[#dadada] shadow">
+      <div className="flex flex-1 justify-center items-center">
+        {/* Left nav */}
+        <nav className="flex flex-row">
+          {navLinksLeft.map((link) => (
+            <NavLink key={link.name} to={link.path} className={navLinkClass}>
+              {link.name}
+            </NavLink>
+          ))}
         </nav>
 
+        {/* Logo */}
         <div className="pl-8 pr-8">
           <img src={uniqueLogo} alt="unique logo" />
         </div>
 
-        <nav className="flex flex-row self-center">
-          <a href="">
-            <p className="pl-8 pr-8">About us</p>
-          </a>
-
-          <a href="">
-            <p className="pl-8 pr-8 text-[#c30022]">Sale</p>
-          </a>
+        {/* Right nav */}
+        <nav className="flex flex-row">
+          {navLinksRight.map((link) => (
+            <NavLink key={link.name} to={link.path} className={navLinkClass}>
+              {link.name}
+            </NavLink>
+          ))}
         </nav>
       </div>
 
-      {/* actions */}
+      {/* Actions */}
       <div className="absolute right-6 top-1/2 transform -translate-y-1/2 flex items-center space-x-4 z-60">
         <button className="rounded hover:bg-gray-100">
           <IoPersonOutline className="w-6 h-6" />
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import "./index.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import Header from "./components/header.tsx";
 import HeroCarousel from "./components/HeroCarousel.tsx";
 import ProductOverview from "./components/ProductOverview.tsx";
@@ -8,15 +9,17 @@ import CollectionOverview from "./components/CollectionOverview.tsx";
 import BestSellerSection from "./components/BestSellerSection.tsx";
 import Footer from "./components/Footer.tsx";
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className="pt-16">
-      <Header />
-      <HeroCarousel />
-      <ProductOverview />
-      <CollectionOverview />
-      <BestSellerSection />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="pt-16">
+        <Header />
+        <HeroCarousel />
+        <ProductOverview />
+        <CollectionOverview />
+        <BestSellerSection />
+        <Footer />
+      </div>
+    </BrowserRouter>
   </StrictMode>
 );
